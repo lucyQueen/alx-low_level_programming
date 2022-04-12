fae0923d9ecd6d3cd138f62e12ecb05ef394dbeb
lucyQueen <ayegbaluccia23@gmail.com>
2022-04-12 14:20:15 -0700

    function that prints 10 times the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *add - adds integers
+  * function that prints 10 times the alphabet, in lowercase
+  *Return: return void
+  */
+int add(int n1, int n2)
+{
+
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
