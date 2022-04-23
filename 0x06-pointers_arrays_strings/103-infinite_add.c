d4346d369e1de77936b605f8847aebdba1a7315b
lucyQueen <ayegbaluccia23@gmail.com>
2022-04-23 13:14:07 -0700

    function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,32 +1,31 @@
-
 #include "main.h"
 #include <stdio.h>
 /**
   *infinite_add - function that adds two numbers
   *@n1: first number
   *@n2: second number
-  *@r:buffer
+  *@r: buffer
   *@size_r: buffer size
   *Return: return dest
   */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int b1 = 0, b2 = 0, op, bg, dr1, dr2, add = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + b1) != '\0')
-		b1++;
-	while (*(n2 + b2) != '\0')
-		b2++;
-	if (b1 >= b2)
-		bg = b1;
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
 	else
-		bg = b2;
+		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
 	r[bg + 1] = '\0';
-	b1--, b2--, size_r--;
-	dr1 = *(n1 + b1) - 48, dr2 = *(n2 + b2) = 48;
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) = 48;
 	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
@@ -38,12 +37,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		*(r + bg) = (op % 10) + 48;
 		else
 			*(r + bg) = '0';
-		if (b1 > 0)
-			b1--, dr1 = *(n1 + b1) - 48;
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
 		else
 			dr1 = 0;
-		if (b2 > 0)
-			b2--, dr2 = *(n2 + b2) - 48;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
 		bg--, size_r--;
