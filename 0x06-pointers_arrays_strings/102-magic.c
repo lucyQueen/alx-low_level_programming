3304dc32d16b88b7d14096468286725fa0b889d3
lucyQueen <ayegbaluccia23@gmail.com>
2022-04-24 12:45:08 -0700

    program prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+
 int main(void)
 {
 	int n;
@@ -9,7 +9,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * write your line of code here..
+	 *main - write your line of code here..
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
