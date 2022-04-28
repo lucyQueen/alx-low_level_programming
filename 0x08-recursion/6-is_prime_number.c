9ceac0d311d05eda835220e67b4951d462c456a9
lucyQueen <ayegbaluccia23@gmail.com>
2022-04-28 14:53:30 -0700

    function that returns 1 if the input integer is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,30 +3,37 @@
 /**
   *prime -  function that returns 1 if the input integer
   *is a prime number, otherwise return
-  *@i:input
-  *@n:iterator
+  *@n:input
+  *@c:iterator
   *
   *Return:return number
   */
 
-int prime(int i, int n)
+int is_prime(unsigned int n, unsigned int c)
 {
-	if (n < 2 || !(n % i))
-		return (0);
-	else if (i > n / 2)
-		return (1);
-	else
-		return (prime(i + 1, n));
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
 }
-
 /**
   *is_prime_number - function that returns 1
   *@n:integer
   *
-  *return: integer
+  *Return: integer
   */
 
 int is_prime_number(int n)
 {
-	return (prime(2, n));
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
 }
