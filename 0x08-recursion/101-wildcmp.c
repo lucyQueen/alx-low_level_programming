7c864cb522b26092612ba78d975faf691cd541dc
lucyQueen <ayegbaluccia23@gmail.com>
2022-04-29 10:45:15 -0700

    function that compares two strings and returns 1 if the strings can be considered identical

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
+/**
+  *strlen_no_wilds - lenght of string
+  *@str:string
+  *
+  *Return:return integer
+  */
+int strlen_no_wilds(char *str)
+{
+	int len = 0, index = 0;
+
+	if (*(str + index))
+	{
+		if (*str != '*')
+			len++;
+
+		index++;
+		len += strlen_no_wilds(str + index);
+	}
+	return (len);
+}
+
+/**
+  *iterate_wild - iterates and points to none wild card
+  *@wildstr:string
+  */
+void iterate_wild(char **wildstr)
+{
+	if (**wildstr == '*')
+	{
+		(*wildstr)++;
+		iterate_wild(wildstr);
+	}
+}
+/**
+  *postfix_match - checks for postfix match
+  *@str:string
+  *@postfix:postfix
+  *
+  *Return:return if string and postring are same
+  */
+char *postfix_match(char *str, char *postfix)
+{
+	int str_len = strlen_no_wilds(str) - 1;
+	int postfix_len = strlen_no_wilds(postfix) - 1;
+
+	if (*postfix == '*')
+		iterate_wild(&postfix);
+
+	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
+	{
+		postfix++;
+		return (postfix_match(str, postfix));
+	}
+
+	return (postfix);
+}
+/**
+  *wildcmp - compare two srtings
+  *@s1:string 1
+  *@s2:string 2
+  *Return:return string if identical
+  */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		iterate_wild(&s2);
+		s2 = postfix_match(s1, s2);
+	}
+
+	if (*s2 == '\0')
+		return (1);
+
+	if (*s1 != *s2)
+		return (0);
+
+	return (wildcmp(++s1, ++s2));
+}
