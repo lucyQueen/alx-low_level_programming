eed88d8bd3b37a27f26636143246bf869f8f9834
lucyQueen <ayegbaluccia23@gmail.com>
2022-05-26 15:55:59 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,40 +1,50 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index -function the Deletes the node at a given
- *                           index of a listint_t list.
- * @head: A pointer to the address of the
- *        head of the listint_t list.
- * @index: The index of the node to be deleted - indices start at 0.
+ * insert_nodeint_at_index - Function insert node
+ * @head: head nodo
+ * @idx: index
+ * @n: integer
  *
- * Return: On success - 1.
- *         On failure - -1.
+ * Return: listint
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp, *copy = *head;
-	unsigned int node;
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *actualNodo = *head;
+	unsigned int i = 0;
 
-	if (copy == NULL)
-		return (-1);
+	if (!new)
+		return (free(new), NULL);
 
-	if (index == 0)
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL && idx > 0)
 	{
-		*head = (*head)->next;
-		free(copy);
-		return (1);
+		free(new);
+		return (NULL);
 	}
 
-	for (node = 0; node < (index - 1); node++)
+	if (idx == 0)
 	{
-		if (copy->next == NULL)
-			return (-1);
-
-		copy = copy->next;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
-	tmp = copy->next;
-	copy->next = tmp->next;
-	free(tmp);
-	return (1);
+	for (; i < idx - 1; i++)
+	{
+		actualNodo = actualNodo->next;
+		if (actualNodo == NULL && idx - i > 0)
+		{
+			free(new);
+			return (NULL);
+		}
+	}
+
+	new->next = actualNodo->next;
+	actualNodo->next = new;
+
+	return (new);
 }
